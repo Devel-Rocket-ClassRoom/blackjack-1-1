5b2e44e7a5bb6635d9116d69dc7bbf0be15a5084
HanuYat <jnjn1235@gmail.com>
2026-02-27 16:32:06 +0900

    블랙잭

---
+++ Blackjack-1/Card.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+class Card
+{
+    public string[,] CardNumber = {
+        {"♦A", "♦2", "♦3", "♦4", "♦5", "♦6", "♦7", "♦8", "♦9", "♦10", "♦J", "♦Q", "♦K"},
+        {"♠A", "♠2", "♠3", "♠4", "♠5", "♠6", "♠7", "♠8", "♠9", "♠10", "♠J", "♠Q", "♠K"},
+        {"♥A", "♥2", "♥3", "♥4", "♥5", "♥6", "♥7", "♥8", "♥9", "♥10", "♥J", "♥Q", "♥K"},
+        {"♣A", "♣2", "♣3", "♣4", "♣5", "♣6", "♣7", "♣8", "♣9", "♣10", "♣J", "♣Q", "♣K"}
+        };
+
+    Random rand = new Random();
+
+    public string ChoiceCard()
+    {
+        while (true)
+        {
+            int randomNumber = rand.Next(0, 52);
+
+            int first = randomNumber / 13;
+            int second = randomNumber % 13;
+
+            string temp = CardNumber[first, second];
+
+            if (temp != "-1")
+            {
+                CardNumber[first, second] = "-1";
+
+                return temp;
+            }
+                     
+        }        
+    }
+}
--- Blackjack-1/Program.cs
+++ Blackjack-1/Program.cs
@@ -1,3 +1,46 @@
 using System;
 
-Console.WriteLine("Hello, Blackjack");
+BlackJack player = new BlackJack("플레이어");
+BlackJack dealer = new BlackJack("딜러");
+
+Card card  = new Card();
+
+Console.WriteLine("=== 블랙잭 게임 ===\n");
+Console.WriteLine("카드를 섞는 중...\n");
+
+player.GameStart(card);
+dealer.GameStart(card);
+
+Console.WriteLine("=== 초기 패 ===");
+dealer.ShowInfo();
+Console.WriteLine();
+player.ShowInfo();
+Console.WriteLine();
+
+while (true)
+{
+    if (player.Total > 21)
+    {
+        break;
+    }
+
+    Console.Write("H(Hit) 또는 S(Stand)를 선택하세요: ");
+    string input  = Console.ReadLine().ToUpper();
+
+    if (input == "H")
+    {
+        player.CardDraw(card);
+        string DrawCard = player.Card.ToString();
+        Console.WriteLine();
+        Console.WriteLine($"{player.Name}가 카드를 받았습니다: [{DrawCard}]");
+        player.ShowInfo();
+        Console.WriteLine();
+    }
+
+    if (input == "S")
+    {
+        Console.WriteLine($"{player.Name}가 Stand를 선택했습니다.");
+        break;
+    }
+}
+
